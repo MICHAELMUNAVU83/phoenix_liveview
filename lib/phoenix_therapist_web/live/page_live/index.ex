52c97c8065bac9848329168c3194957a24a20495
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-07-09 18:12:42 +0300

    Added

--- lib/phoenix_therapist_web/live/page_live/index.ex
+++ lib/phoenix_therapist_web/live/page_live/index.ex
@@ -1,7 +1,20 @@
 defmodule PhoenixTherapistWeb.PageLive.Index do
   use PhoenixTherapistWeb, :live_view
+  alias PhoenixTherapist.Accounts
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, :page_title, "Home")}
+  def mount(_params, session, socket) do
+    if session["user_token"] != nil do
+      user = Accounts.get_user_by_session_token(session["user_token"])
+
+      {:ok,
+       socket
+       |> assign(:page_title, "Home")
+       |> assign(:user, user)}
+    else
+      {:ok,
+       socket
+       |> assign(:page_title, "Home")
+       |> assign(:user, nil)}
+    end
   end
 end
